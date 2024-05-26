6e27845751533d7f98c818faf97ad24d6c184ac9
juu7713 <104571267+juu7713@users.noreply.github.com>
2024-05-26 15:03:35 +0900

    플레이어 추락시 텔레포트용 스크립트, 프리팹 추가
    TeleportationPos(Prefab): 플레이어가 순간이동 되는 목표지점 오브젝트
    FallingCatcher(Prefab): 플레이어의 추락 포착, 플레이어 충돌시  텔레포트 지점으로 순간이동. Teleportation 스크립트 포함
    Teleportation(Script): 플레이어 텔레포트 실행

--- UnityGameProject/Assets/Script/Teleportation.cs
+++ UnityGameProject/Assets/Script/Teleportation.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class Teleportation : MonoBehaviour
 {
-    void OnCollisionEnter(Collider other)
+    public GameObject player;   // 플레이어 오브젝트
+    public GameObject telePos;  // 텔레포트 목표점 오브젝트
+
+    void OnTriggerEnter(Collider other) 
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player")      // 플레이어 충돌시
         {
-            // 플레이어 순간이동
+                Vector3 Pos = telePos.transform.position;    // 텔레포트 위치 = telePos 오브젝트의 위치
+                Pos += Vector3.up * 2;      // 위치 보정
+                player.transform.position = Pos;     // 텔레포트 위치로 플레이어 이동   
         }
     }
-    void Update()
-    {
-            
-    }
+    
 }
 
